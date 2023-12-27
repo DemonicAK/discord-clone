245f360cf1263a196bdc319d8ab6a6b8327dd037
arijitkar <demonicak07@gmail.com>
2023-12-28 00:10:30 +0530

    too much work

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,9 @@
-import React from "react";
-
-const Authlayout = ({ children }: { children: React.ReactNode }) => {
-  return <div className="bg-blue-700 h-full flex items-center justify-center">{children}</div>;
-};
-
-export default Authlayout;
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return ( 
+    <div className="h-full flex items-center justify-center">
+      {children}
+    </div>
+   );
+}
+ 
+export default AuthLayout;
