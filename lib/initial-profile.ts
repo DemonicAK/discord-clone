cfdd542e51975ef72528db6c8cfd9ddd56f82ea3
ArijitKar <demonicak07@gmail.com>
2024-09-07 23:54:41 +0530

    some updates

--- lib/initial-profile.ts
+++ lib/initial-profile.ts
@@ -1,18 +1,25 @@
-import { currentUser, redirectToSignIn } from "@clerk/nextjs";
+// import { currentUser, redirectToSignIn } from "@clerk/nextjs";
+import { currentUser, redirectToSignIn } from "@clerk/nextjs/server";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+// import { useAuth } from "@clerk/clerk-react";
 
 import { db } from "@/lib/db";
 
 export const initialProfile = async () => {
   const user = await currentUser();
+    // const { user } = useAuth();
+  const { userId } = auth();
 
   if (!user) {
-    return redirectToSignIn();
+    // return redirectToSignIn();
+        redirect("/sign-in");
   }
 
   const profile = await db.profile.findUnique({
     where: {
-      userId: user.id
-    }
+      userId: user.id,
+    },
   });
 
   if (profile) {
@@ -24,8 +31,8 @@ export const initialProfile = async () => {
       userId: user.id,
       name: `${user.firstName} ${user.lastName}`,
       imageUrl: user.imageUrl,
-      email: user.emailAddresses[0].emailAddress
-    }
+      email: user.emailAddresses[0].emailAddress,
+    },
   });
 
   return newProfile;
--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,25 @@
-import { authMiddleware } from "@clerk/nextjs";
+// import { authMiddleware } from "@clerk/nextjs";
  
-// This example protects all routes including api/trpc routes
-// Please edit this to allow other routes to be public as needed.
-// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
-export default authMiddleware({
-  publicRoutes: ["/api/uploadthing"]
-});
+// // This example protects all routes including api/trpc routes
+// // Please edit this to allow other routes to be public as needed.
+// // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
+// export default authMiddleware({
+//   publicRoutes: ["/api/uploadthing"]
+// });
  
+// export const config = {
+//   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
+
+import { clerkMiddleware } from '@clerk/nextjs/server'
+
+export default clerkMiddleware()
+
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-};
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+}
